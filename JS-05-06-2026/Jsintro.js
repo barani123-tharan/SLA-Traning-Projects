9ada6eadb6786253118ca32f7260e46e688f261b
barani123-tharan <baranitharantamilselvam@gmail.com>
2026-06-11 16:44:01 +0530

    Add social media assets and task files

--- JS-05-06-2026/Jsintro.js
+++ JS-05-06-2026/Jsintro.js
@@ -67,3 +67,85 @@ names.unshift("nandha");
 names.unshift("jaya");  // unshift used to add items in array at first 
 names.shift();  // it remove first items in array
 console.log("names:"+names);
+
+//reduce
+
+let boy=[1,3,4,5,];
+console.log(boy.reduce((total,current)=>total+current,0))
+
+// find
+
+let number=[1,3,4,5];
+console.log(number.find(find=> find==4));
+
+// function decleration
+
+function man(){
+    console.log('men');
+}
+man ();
+
+// function expression
+
+const add=function(){
+    let name = "barani";
+    console.log(name);
+
+    a=2;
+    b=5;
+    console.log(a+b);
+}
+add();
+
+
+// nested function 
+
+function men(){
+    console.log("men is powerfull");
+
+    function women(){
+        console.log("women is powerfull");
+    }
+    women();
+}
+ men();
+
+ // parameter function
+
+function addtowname(){
+    return "baraani" + "tharan";
+}
+console.log(addtowname());
+
+//return function
+
+function returnf(){
+    console.log("return function");
+    return function numbers(){
+        console.log(10,20,50);
+    }
+}
+returnf()();
+
+
+// rest parameter function  --- used to store values in an array
+
+function go(...num){        // here this ... consider as a paramater 
+    console.log(num);
+
+}
+go(10,20,40);
+
+
+// arrow function  -- used to create logic
+
+
+const ad=(a,b)=>a+b;
+console.log(add(10,20),add(30,40))
+const date=(a,b)=>{
+    console.log("helo");
+
+    return a+b;
+
+}
+console.log(date(10,20));
