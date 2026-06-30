11a66bcd2f429eb4f6fb8d2315217c9fa32d7ae5
barani123-tharan <baranitharantamilselvam@gmail.com>
2026-06-30 22:57:35 +0530

    commited

---
+++ JS-05-06-2026/loops.js
@@ -0,0 +1,20 @@
+// Loops
+
+// While , do-while , for loop
+// -- Initialistion , Validation , updation  --      important line 
+// While Loop
+
+// loop oda condition eppo false agu tho apo loop haa vittu veliya vandhu ruum 
+// loop false aga laa naa infinite haa run agitaeyyy erukum 
+let num = 1;
+ while (num <= 10){
+    console.log(num);
+    num++;
+ 
+ }
+ console.log("Condition False");
+
+
+ // do-while Loop
+
+ 
